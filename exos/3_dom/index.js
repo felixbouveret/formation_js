afdcfccd951b55009887bc6515254bd8d8d9c579
Félix Bouveret <bouveret.felix@gmail.com>
2020-11-12 17:00:49 +0100

    Add dom exercice

--- exos/3_dom/index.js
+++ exos/3_dom/index.js
@@ -1,6 +1,15 @@
-'use strict';
+"use strict";
 
 // # 4_DOM
+
+const createSection = (exoNumber) => {
+  const body = document.querySelector("body");
+  const section = document.createElement("section");
+  section.setAttribute("id", `exo${exoNumber}`);
+  body.appendChild(section);
+
+  return section;
+};
 
 /* Exercice 1: Couleurs
     - Créer une <div> pour chaque couleur, avec la couleur en textContent, et l'ajouter à l'élément avec l'id 'exo1'
@@ -8,7 +17,19 @@
     - Au click, chaque div doit logguer sa couleur dans la console
 */
 
-const colors = ['blue', 'red', 'green', 'black', 'grey', 'orange', 'purple'];
+const colors = ["blue", "red", "green", "black", "grey", "orange", "purple"];
+
+const parent = document.querySelector("#exo1");
+
+colors.forEach((color) => {
+  const colorBlock = document.createElement("div");
+  colorBlock.textContent = color;
+
+  colorBlock.style.backgroundColor = color; // Wrong way to do
+  colorBlock.addEventListener("click", () => console.log(color));
+
+  parent.appendChild(colorBlock);
+});
 
 // -------------------------------
 
@@ -19,6 +40,20 @@ const colors = ['blue', 'red', 'green', 'black', 'grey', 'orange', 'purple'];
     en fonction de la position de la souris à l'écran (event.clientX, event.clientY)
 */
 
+const section2 = createSection(2);
+
+const squareDiv = document.createElement("div");
+squareDiv.style.backgroundColor = "black";
+squareDiv.style.width = "100px";
+squareDiv.style.height = "100px";
+
+section2.appendChild(squareDiv);
+
+squareDiv.addEventListener("mousemove", function (e) {
+  this.style.width = `${e.clientX}px`;
+  this.style.height = `${e.clientY}px`;
+});
+
 // -------------------------------
 
 /* Exercice 3: Harry & friends
@@ -28,7 +63,24 @@ const colors = ['blue', 'red', 'green', 'black', 'grey', 'orange', 'purple'];
     puis remplace la <div> cliquée par une nouvelle <div>, avec le nouveau nom
 */
 
-const names = ['Harry', 'Hermione', 'Ron', 'Sirius', 'Hagrid', 'Albus'];
+const names = ["Harry", "Hermione", "Ron", "Sirius", "Hagrid", "Albus"];
+
+const section3 = createSection(3);
+
+const createNameBlock = (nameIndex) => {
+  const nameBlock = document.createElement("div");
+
+  nameBlock.textContent = names[nameIndex];
+  nameBlock.classList.add(names[nameIndex]);
+  section3.appendChild(nameBlock);
+
+  nameBlock.addEventListener("click", function () {
+    nameBlock.remove();
+    createNameBlock(Math.floor(Math.random() * names.length));
+  });
+};
+
+createNameBlock(0);
 
 // -------------------------------
 
@@ -39,6 +91,40 @@ const names = ['Harry', 'Hermione', 'Ron', 'Sirius', 'Hagrid', 'Albus'];
     de la position de la souris dans la fenêtre (event.clientX, event.clientY)
 */
 
+const section4 = createSection(4);
+
+const trackButtonText = {
+  on: "Tracking On",
+  off: "Tracking Off",
+};
+
+const trackButton = document.createElement("button");
+trackButton.textContent = trackButtonText.off;
+let isButtonActive = false;
+
+const trackingFigures = document.createElement("span");
+
+const trackingFunction = (event) => {
+  console.log(event.clientX, event.clientY);
+  trackingFigures.textContent = `${event.clientX}px, ${event.clientY}px`;
+};
+
+trackButton.addEventListener("click", function () {
+  if (isButtonActive) {
+    document.removeEventListener("mousemove", trackingFunction);
+    this.textContent = trackButtonText.off;
+    trackingFigures.remove();
+  } else {
+    document.addEventListener("mousemove", trackingFunction);
+    this.textContent = trackButtonText.on;
+    section4.appendChild(trackingFigures);
+  }
+
+  isButtonActive = !isButtonActive;
+});
+
+section4.appendChild(trackButton);
+
 // -------------------------------
 
 /* Exercice Bonus: Click and drag
@@ -48,3 +134,26 @@ const names = ['Harry', 'Hermione', 'Ron', 'Sirius', 'Hagrid', 'Albus'];
       * quand on clique dessus en laissant enfoncé, la <div> se déplace en fonction des déplacements de la souris
       * lorsqu'on relâche, la <div> ne se déplace plus
 */
+
+const section5 = createSection(5);
+
+const dragDiv = document.createElement("div");
+dragDiv.textContent = "Drag me";
+dragDiv.style.backgroundColor = "black";
+dragDiv.style.width = "100px";
+dragDiv.style.height = "100px";
+
+const drag = (e) => {
+  dragDiv.style.left = `${e.clientX - 50}px`;
+  dragDiv.style.top = `${e.clientY - 50}px`;
+};
+
+dragDiv.addEventListener("mousedown", () => {
+  dragDiv.style.position = "absolute";
+  dragDiv.addEventListener("mousemove", drag);
+});
+dragDiv.addEventListener("mouseup", (e) => {
+  dragDiv.removeEventListener("mousemove", drag);
+});
+
+section5.appendChild(dragDiv);
